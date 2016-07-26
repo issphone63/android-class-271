4364298c67fa16486b98c72eebee36fb51c93f87
FrankLikuohao <frank.likuohao@gmail.com>
2016-07-26 10:35:57 +0800

    update

--- simapleui/app/src/main/java/tw/com/a_team/simapleui/MainActivity.java
+++ simapleui/app/src/main/java/tw/com/a_team/simapleui/MainActivity.java
@@ -2,13 +2,25 @@ package tw.com.a_team.simapleui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textView=(TextView)findViewById(R.id.textView1);
+
+    }
+
+    public void submit(View view)
+    {
+
+        textView.setText("Hello Android");
     }
 }
